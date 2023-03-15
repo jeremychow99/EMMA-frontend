90a1baf06a3f6a0315a40370eac10110d0ceebce
jeremy chow <xelloxzxd@gmail.com>
2023-03-15 15:17:38 +0800

    add vuetify

--- frontend/src/components/HelloWorld.vue
+++ frontend/src/components/HelloWorld.vue
@@ -1,15 +1,16 @@
 <script setup>
-import { ref } from 'vue'
+import { ref } from "vue";
 
 defineProps({
   msg: String,
-})
+});
 
-const count = ref(0)
+const count = ref(0);
 </script>
 
 <template>
   <h1>{{ msg }}</h1>
+  <v-btn> Button </v-btn>
 
   <div class="card">
     <button type="button" @click="count++">count is {{ count }}</button>
