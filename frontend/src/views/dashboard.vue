3e11a0d5338b441faa0c934e12a2026bc0a7896f
adamtwh <111948686+adamtwh@users.noreply.github.com>
2023-03-28 15:12:54 +0800

    table overview of equipment status

--- frontend/src/views/dashboard.vue
+++ frontend/src/views/dashboard.vue
@@ -2,8 +2,46 @@
   <navbar></navbar>
 
   <v-container>
+    <div class="text-h4 font-weight-bold">Equipment Overview</div>
+    <v-table
+    fixed-header
+    height="300px"
+    >
+      <thead>
+        <tr>
+          <th class="text-left">
+            ID
+          </th>
+          <th class="text-left">
+            Name
+          </th>
+          <th class="text-left">
+            Location
+          </th>
+          <th class="text-left">
+            Status
+          </th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr
+          v-for="eq in equipment_arr"
+          :key="eq._id"
+        >
+          <td>{{ eq._id }}</td>
+          <td>{{ eq.equipment_name }}</td>
+          <td>{{ eq.equipment_location }}</td>
+          <td>{{ eq.equipment_status }}</td>
+        </tr>
+      </tbody>
+    </v-table>
+  
+  </v-container>
 
   <!-- Equipment Status Card -->
+  
+  <v-container>
+    <v-divider class="mb-5"></v-divider>
     <v-card
       title="Equipment Status"
       subtitle="Proportion of Operational Equipment"
@@ -86,39 +124,83 @@ export default {
   data() {
       return {
         // values to be calculated
-        // equipment_arr: [],
-        // eqNoMaintain:
-        // eqMaintain:
+        equipment_arr: [],
+        eqNoMaintain: 0,
+        eqMaintain: 0,
 
         // test values
-        eqOperational: 87,
-        eqWarning: 10,
-        eqDown: 3,
+        // eqOperational: 87,
+        // eqWarning: 10,
+        // eqDown: 3,
 
         showReport: false,
+
+        //  test values
+        // desserts: [
+        //   {
+        //     name: 'Frozen Yogurt',
+        //     calories: 159,
+        //   },
+        //   {
+        //     name: 'Ice cream sandwich',
+        //     calories: 237,
+        //   },
+        //   {
+        //     name: 'Eclair',
+        //     calories: 262,
+        //   },
+        //   {
+        //     name: 'Cupcake',
+        //     calories: 305,
+        //   },
+        //   {
+        //     name: 'Gingerbread',
+        //     calories: 356,
+        //   },
+        //   {
+        //     name: 'Jelly bean',
+        //     calories: 375,
+        //   },
+        //   {
+        //     name: 'Lollipop',
+        //     calories: 392,
+        //   },
+        //   {
+        //     name: 'Honeycomb',
+        //     calories: 408,
+        //   },
+        //   {
+        //     name: 'Donut',
+        //     calories: 452,
+        //   },
+        //   {
+        //     name: 'KitKat',
+        //     calories: 518,
+        //   },
+        // ],
       }
   },
 
   // have not tested yet!
-  // methods: {
-  //   updateEquipmentStatus() {
-  //     for (equipment in this.equipment_arr) {
-  //       if (equipment.equipment_status == "No maintenance required") {
-  //         this.eqNoMaintain++
-  //       }
-  //       else {
-  //         this.eqMaintain++
-  //       }
-  //     }
-  //   },
+  methods: {
+    updateEquipmentStatus() {
+      for (equipment in this.equipment_arr) {
+        if (equipment.equipment_status == "No maintenance required") {
+          this.eqNoMaintain++
+        }
+        else {
+          this.eqMaintain++
+        }
+      }
+    },
 
-  // },
+  },
 
-  // async mounted() {
-  //       let eqp_result = await axios.get(equipmentURL)
+  async mounted() {
+        let eqp_result = await axios.get(equipmentURL)
 
-  //       this.equipment_arr = eqp_result.data.data.equipment
-  //   }
+        this.equipment_arr = eqp_result.data.data.equipment
+    }
 
   
 };
