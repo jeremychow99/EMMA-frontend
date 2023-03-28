911b631c08963aa816348afe3e1fa11e9ad1267e
Deborah How <hyh.deb@gmail.com>
2023-03-28 16:41:00 +0800

    able to load all maintenance records and start maintenance (but have not resolved datetime issue)

--- frontend/src/views/startMaintenance.vue
+++ frontend/src/views/startMaintenance.vue
@@ -24,48 +24,76 @@
         </v-row>
         
         <v-row style="margin-bottom: 10px;">
-            <select name="" id="" style="margin-left: 20px; padding: 0px 10px; border: lightgrey 1px solid;">
-                <option value="" disabled selected >Please select</option>
-                <v-for>
-                    <option value=""></option>
-                </v-for>
+            <select v-model="selectedRecord" style="margin-left: 20px; padding: 0px 10px; border: lightgrey 1px solid;">
+                <option disabled selected >Please select</option>
+                <option v-for="maintenance in maintenance_arr" :value="maintenance">{{ maintenance._id }}</option>
             </select>
         </v-row>
+        <!-- <h1>{{selectedRecord}}</h1> -->
+        
 
         <v-row style="padding-left: 20px;">
-            <v-table>
+            <v-table v-if="selectedRecord == ''">
                 <thead>
                 <tr>
                     <th class="text-left">
                     ID
                     </th>
                     <th class="text-left">
-                    Description
+                    Status
                     </th>
                 </tr>
                 </thead>
                 <tbody>
                 <tr
-                    v-for="maintenance in maintenance_arr"
-                    :key="maintenance.id"
+                v-for="(maintenance, index) in maintenance_arr" :key="maintenance._id" 
                 >
-                    <td>{{ maintenance.id }}</td>
-                    <td>{{ maintenance.status }}</td>
+                    <td v-if="index < 5">{{ maintenance._id }}</td>
+                    <td v-if="maintenance.status && index < 5">{{ maintenance.status }}</td>
+                    <td v-else v-if="index <5"> NA </td>
                 </tr>
                 </tbody>
             </v-table>
+
+            
+            <!--DISPLAY PARTS INVENTORY-->
+            <v-div v-else>
+                <h2>Maintenance Record ID: {{ selectedRecord._id }} </h2>
+                <h3>Equipment ID: {{ selectedRecord.equipment_id }}</h3>
+                <!-- <h3>Current Status: {{ status }}</h3>
+                <h3>Scheduled for: {{ }}</h3>  -->
+
+                <v-table>
+                    <thead>
+                        <tr>
+                            <th>
+                                Parts
+                            </th>
+                            <th>
+                                Quantity Reserved
+                            </th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr v-for="detail in selectedRecord.partlist">
+                            <td>
+                                {{ detail.PartName }}
+                            </td>
+                            <td>
+                                {{ detail.ReservedQty }}
+                            </td>
+                        </tr>
+                    </tbody>
+                </v-table>
+            </v-div>
+            
         </v-row>
 
         <v-row class="justify-end">
             <!-- <v-btn style="background-color: #5D5FEF; color: white; margin-right: 10px;">
                 Previous
             </v-btn> -->
-            <!-- STEP 1: CHOOSE MAINTENANCE RECORD
-                STEP 2: REPLACE PLEASE SELECT DROP DOWN WITH MAINTENANCE RECORD DETAILS 
-                - EQUIPMENTID, STATUS, SCHEDULEDATETIME
-                STEP 3: DISPLAY PARTS INVENTORY
-            -->
-            <v-btn style="background-color: #5D5FEF; color: white;">
+            <v-btn @click="update_maintenance_status" style="background-color: #5D5FEF; color: white;">
                 Start
             </v-btn>
         </v-row>
@@ -77,7 +105,7 @@
 
 <script>
 import navbar from "../components/navbar.vue"
-import { equipmentURL, inventoryURL , maintenanceURL} from '../../api'
+import { equipmentURL, maintenanceURL, maintenanceControllerURL} from '../../api'
 import axios from "axios";
 
 export default {
@@ -87,25 +115,48 @@ export default {
 
     data() {
         return {
+            selectedRecord: "",
+            status: "NA",
             maintenance_arr: [],
-            equipment_arr: [],
-            inventory_arr: [],
+
+
         }
     },
 
     methods: {
-
+        update_maintenance_status(){
+            let datetime = new Date().toLocaleDateString("en-GB")
+            console.log(datetime)
+            let data = {
+                "equipment_id": this.selectedRecord.equipment_id,
+                // doesnt work when i put current time but hardcoded is ok
+                "start_datetime": datetime
+            }
+            console.log(data);
+            axios.put(`${maintenanceControllerURL}/start_maintenance/${this.selectedRecord._id}`, data, {
+                    headers: {
+                        "Content-Type": "application/json",
+                        "Access-Control-Allow-Origin": "*",
+                        Authorization: "Bearer " + localStorage.token,
+                    }
+                }).then(response => {
+                    console.log(response.data)
+                    alert("Success")
+                }).catch(error =>{
+                    console.error(error)
+                    alert("Failed")
+                })
+        }
     },
 
+    
     async mounted() {
-        let eqp_result = await axios.get(equipmentURL)
-        let inv_result = await axios.get(inventoryURL)
         let maint_result = await axios.get(maintenanceURL)
-
         this.maintenance_arr = maint_result.data.data.maintenance
-        this.equipment_arr = eqp_result.data.data.equipment
-        this.inventory_arr = inv_result.data.data.parts
+        // console.log(this.maintenance_arr)
     }
+
+    
 };
 
 </script>
