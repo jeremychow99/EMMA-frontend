34b8b01fb91b3beaddea68f6fd9f158b017218bc
Deborah How <hyh.deb@gmail.com>
2023-03-31 16:56:40 +0800

    start and end maintenance in one vue file

--- frontend/src/views/startMaintenance.vue
+++ frontend/src/views/startMaintenance.vue
@@ -10,54 +10,8 @@
             <v-col cols="8">
             </v-col>
         </v-row>
-        
-        <v-row>
-            <v-col cols="4">
-                <span style="color: #5D5FEF; font-weight: bold">Select a maintenance record:</span>
-            </v-col>
-        </v-row>
-        
-        <v-row>
-            <v-col style="padding-left: 20px;">
-                <h5>Maintenance Record ID</h5>
-            </v-col>
-        </v-row>
-        
-        <v-row style="margin-bottom: 10px;">
-            <select v-model="selectedRecord" style="margin-left: 20px; padding: 0px 10px; border: lightgrey 1px solid;">
-                <option disabled selected >Please select</option>
-                <option v-for="maintenance in maintenance_arr" :value="maintenance">{{ maintenance._id }}</option>
-            </select>
-        </v-row>
-        <!-- <h1>{{selectedRecord}}</h1> -->
-        
-
-        <v-row style="padding-left: 20px;">
-            <v-table v-if="selectedRecord == ''">
-                <thead>
-                <tr>
-                    <th class="text-left">
-                    ID
-                    </th>
-                    <th class="text-left">
-                    Status
-                    </th>
-                </tr>
-                </thead>
-                <tbody>
-                <tr
-                v-for="(maintenance, index) in maintenance_arr" :key="maintenance._id" 
-                >
-                    <td v-if="index < 5">{{ maintenance._id }}</td>
-                    <td v-if="maintenance.status && index < 5">{{ maintenance.status }}</td>
-                    <td v-else v-if="index <5"> NA </td>
-                </tr>
-                </tbody>
-            </v-table>
-
-            
-            <!--DISPLAY PARTS INVENTORY-->
-            <v-div v-else>
+        <!-- <router-link v-if="this.$route.params.id == this.$store.state.user.email" class="text-white position-absolute fs-3" :to="{ name: 'Setup' }" style="width: 30px; top: 30px; right:60px"><i class="fa-solid fa-pen rounded-circle border border-white border-3 p-2"></i></router-link> -->
+            <v-div v-if="selectedRecord.status == 'SCHEDULED' ">
                 <h2>Maintenance Record ID: {{ selectedRecord._id }} </h2>
                 <h3>Equipment ID: {{ selectedRecord.equipment_id }}</h3>
                 <!-- <h3>Current Status: {{ status }}</h3>
@@ -80,23 +34,107 @@
                                 {{ detail.PartName }}
                             </td>
                             <td>
-                                {{ detail.ReservedQty }}
+                                {{ detail.Qty }}
                             </td>
                         </tr>
                     </tbody>
-                </v-table>
+                </v-table>  
+                
+                <v-row class="justify-end">
+                    <!-- <v-btn style="background-color: #5D5FEF; color: white; margin-right: 10px;">
+                        Previous
+                    </v-btn> -->
+                    <v-btn @click="update_maintenance_status" style="background-color: #5D5FEF; color: white;">
+                        Start
+                    </v-btn>
+                </v-row>  
             </v-div>
             
-        </v-row>
+            <v-container v-else-if="selectedRecord.status =='STARTED'">
+                <v-row>
+                    <v-col>
+                        <h2>Maintenance Record ID: {{ selectedRecord._id }}</h2>
+                    </v-col>
+                </v-row>
 
-        <v-row class="justify-end">
-            <!-- <v-btn style="background-color: #5D5FEF; color: white; margin-right: 10px;">
-                Previous
-            </v-btn> -->
-            <v-btn @click="update_maintenance_status" style="background-color: #5D5FEF; color: white;">
-                Start
-            </v-btn>
-        </v-row>
+                <v-row style="margin-top: 0">
+                    <v-col>
+                        <h3>Equipment ID: {{ selectedRecord.equipment_id }} </h3>
+                    </v-col>
+                </v-row>
+
+                <v-row style="margin-top: 0">
+                    <v-col>
+                        <h3>Current status: {{ selectedRecord.status }} </h3>
+                    </v-col>
+                </v-row>
+
+                <v-row style="margin-top: 0">
+                    <v-col>
+                        <h3>Scheduled for: {{ selectedRecord.last_maintained }}</h3>
+                    </v-col>
+                </v-row>
+
+                <v-row>
+                    <v-col>
+                        <h3>Unused Parts</h3>
+                    </v-col>
+                </v-row>
+
+                <v-row>
+                    <v-table>
+                        <thead>
+                            <tr>
+                                <th class="text-left">
+                                Part Name
+                                </th>
+                                <th class="text-left">
+                                Qty
+                                </th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr
+                            v-for="parts in selectedRecord.partlist"
+                            :key="parts"
+                            >
+                                <td>{{ parts.PartName }}</td>
+                                <td>
+                                    <input type="text" :id="parts['_id']" :name="parts.PartName" placeholder="Number of unused parts" @change="update_unused_partlist_array($event.currentTarget)">
+                                </td>
+                            </tr>
+                        </tbody>
+                    </v-table>
+                </v-row>
+
+                <v-row>
+                    <v-col cols="4">
+                        <h3>Status</h3>
+                    </v-col>
+                </v-row>
+
+                <v-row>
+                    <input type="radio" :value="'COMPLETE - SUCCESSFUL'" v-model="status"> Completed Successfully
+                </v-row>
+                
+                <v-row>
+                    <input type="radio" :value="'COMPLETE - UNSUCCESSFUL'" v-model="status"> Not Successful
+                </v-row>
+
+                <v-row>
+                    <v-col cols="4">
+                        <h3>Additional Comments</h3>
+                        <v-text-field v-model="description"></v-text-field>
+                    </v-col>
+                </v-row>
+                <v-row class="justify-end">
+                    <v-btn @click="end_of_maintenance" style="background-color: #5D5FEF; color: white;">
+                        End
+                    </v-btn>
+                </v-row>  
+
+                {{ new Date().toISOString() }}
+            </v-container>
     </v-container>
 
 </template>
@@ -105,7 +143,7 @@
 
 <script>
 import navbar from "../components/navbar.vue"
-import { equipmentURL, maintenanceURL, maintenanceControllerURL} from '../../api'
+import { maintenanceURL, maintenanceControllerURL, inventoryURL } from '../../api'
 import axios from "axios";
 
 export default {
@@ -116,19 +154,20 @@ export default {
     data() {
         return {
             selectedRecord: "",
-            status: "NA",
-            maintenance_arr: [],
-
-
+            description: "",
+            status: "",
+            unusedPartList: [],
+            current_date: new Date().toISOString(),
         }
     },
 
     methods: {
         update_maintenance_status(){
+            console.log("=== START update_maintenance_status ===")
             let datetime = new Date().toLocaleDateString("en-GB")
             console.log(datetime)
             let data = {
-                "equipment_id": this.selectedRecord.equipment_id,
+                "equipment_id": this.selectedRecord.equipment.equipment_id,
                 // doesnt work when i put current time but hardcoded is ok
                 "start_datetime": datetime
             }
@@ -146,13 +185,109 @@ export default {
                     console.error(error)
                     alert("Failed")
                 })
+        }, 
+
+        update_unused_partlist_array(e){
+            console.log(e)
+            let part_id = e.id
+            let unused_qty = e.value
+            let part_name = e.name
+            
+            let partObj = {
+                "PartName": part_name,
+                "Qty": unused_qty,
+                "_id": part_id
+            }
+
+            let temp_part_list = this.unusedPartList
+            let part_idx = temp_part_list.findIndex(part => part["_id"] === part_id)
+
+            console.log(part_idx)
+
+            if (unused_qty === "0") {
+                temp_part_list = temp_part_list.filter(part => part["_id"] != part_id)
+            } else if (part_idx == -1) {
+                console.log("Added Parts")
+                temp_part_list.push(partObj)
+            } else {
+                temp_part_list[part_idx]["Qty"] = reserve_qty
+            }
+
+            this.unusedPartList = temp_part_list
+        }, 
+
+        // async update_inventory_parts(){
+        //     await axios.post(`${inventoryURL}/inventory/return}`, partObj, {
+        //             headers: {
+        //                 "Content-Type": "application/json",
+        //                 "Access-Control-Allow-Origin": "*",
+        //                 Authorization: "Bearer " + localStorage.token,
+        //             }
+        //         })
+        //         .then(response => {
+        //             console.log(response.data)
+        //             console.log("Updating inventory")
+        //         })
+        //         .catch(error=>{
+        //             console.log(error)
+        //             alert("did not update")
+        //         })
+        // },
+
+        async end_of_maintenance(){
+            console.log("=== START end_of_maintenance ===")
+            var datetime_arr = this.current_date.split("T");
+            var date = datetime_arr[0]
+            var time = datetime_arr[1]
+            var date_arr = date.split("-")
+
+            var month = date_arr[1]
+            var day = date_arr[2]
+            var year = date_arr[0]
+
+            // console.log(month)
+            // console.log(day)
+            // console.log(year)
+
+            this.current_date = day + '/' + month + '/' + year + " " + time + ":00"
+            console.log(this.current_date)
+
+            let data = {
+                "equipment": this.selectedRecord.equipment,
+                "schedule_date": this.selectedRecord.schedule_date,
+                // "schedule_date": "29/03/2023",
+                "start_datetime": this.selectedRecord.equipment.start_datetime,
+                // "start_datetime": "30-03-2023 17:30:00",
+                "partlist": this.selectedRecord.partlist,
+                "return_partlist": this.unusedPartList,
+                // "end_datetime": "31-03-2023 17:30:00",
+                "end_datetime": this.current_date,
+                "description": this.description,
+                "maintenance_status": this.status
+            }
+            console.log(data)
+            axios.put(`${maintenanceControllerURL}/end_maintenance/${this.selectedRecord._id}`, data, {
+                    headers: {
+                        "Content-Type": "application/json",
+                        "Access-Control-Allow-Origin": "*",
+                        Authorization: "Bearer " + localStorage.token,
+                    }
+                }).then(response => {
+                    console.log(response.data)
+                    alert("Successfully ended maintenance")
+                }).catch(error =>{
+                    console.error(error)
+                    alert("Failed to end maintenance")
+                })
         }
     },
 
     
     async mounted() {
-        let maint_result = await axios.get(maintenanceURL)
-        this.maintenance_arr = maint_result.data.data.maintenance
+        let maintenance_id = this.$route.params.id
+        let maint_result = await axios.get(`${maintenanceURL}/${maintenance_id}`)
+        console.log(maint_result)
+        this.selectedRecord = maint_result.data.data
         // console.log(this.maintenance_arr)
     }
 
